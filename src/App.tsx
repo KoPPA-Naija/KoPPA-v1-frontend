0f67907fca3850d940b15f8cb63732a0d3ddf184
talentlessDeveloper <kareemope52@gmail.com>
2023-07-17 08:31:08 +0100

    scaffolded app with vite , airbnb typescript eslint, tailwind and react-router

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="mt-10 bg-green-400 py-9">
+      <h1 className="mx-auto max-w-sm text-center text-3xl text-white md:text-6xl">
+        Welcome To <br /> KoPPA Rebuild <br /> 🎈💭🎈🎈🎇🎉
+      </h1>
+    </div>
+  );
+}
+
+export default App;
